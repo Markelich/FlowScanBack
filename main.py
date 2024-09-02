7c00b804cf353f676a8acdeafed9ceccd15a57a4
Markelich <Zaworotniy.marck@yandex.ru>
2024-09-02 15:43:23 +0900

    added domain pars

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ router = APIRouter(
 
 
 @router.get("/{hosts}/{startTime}/{finalTime}")
-def get_ip_list(startTime: str, finalTime: str):
+async def get_ip_list(startTime: str, finalTime: str):
     query = f"""SELECT DstIP, SUM(Pkts) AS Pkts, SUM(Octets) AS Octets FROM `INPUT`
         WHERE (DstIP BETWEEN '10.1.0.0' and '10.2.0.0' OR DstIP BETWEEN '192.168.0.0' and '192.169.0.0')
         AND `Start` BETWEEN '{startTime}:00' AND '{finalTime}:00' GROUP BY DstIP
@@ -48,16 +48,17 @@ def get_ip_list(startTime: str, finalTime: str):
     return df
 
 @router.get("/domain_names/{server_addres}")
-def get_domain_name(server_addres):
+async def get_domain_name(server_addres):
     try:
         domain = socket.gethostbyaddr(server_addres)[0]
-        return domain
     except socket.herror:
-        return 'Unknown'
+        domain = 'Unknown'
+    # print(domain)
+    return domain
 
 
 @router.get("/hosts/{user_addres}/{startTime}/{finalTime}")
-def get_serveses_list(user_addres, startTime: str, finalTime: str):
+async def get_serveses_list(user_addres, startTime: str, finalTime: str):
     print(user_addres)
     query = f"""SELECT SrcIP, DstIP, SUM(Pkts) AS Pkts, SUM(Octets) AS Octets FROM `INPUT`
             WHERE DstIP = '{user_addres}'
@@ -73,7 +74,25 @@ def get_serveses_list(user_addres, startTime: str, finalTime: str):
     df = df.reset_index().to_dict('records')
     return df
 
+@router.get("/curentData")
+async def get_last_data():
+    queryMainGrapf = f"""WITH 
+                    (SELECT max(`Start`) FROM `INPUT`) AS latest_timestamp
+                SELECT Start, Octets
+                FROM `INPUT`
+                WHERE `Start` >= latest_timestamp - INTERVAL 15 MINUTE
+                ORDER BY `Start`;"""
+    print(queryMainGrapf)
+    query_resultMainGrapf = client.query(queryMainGrapf)
+    setMainGrapf = (query_resultMainGrapf.result_set)
 
+    dfMainGrapf = pd.DataFrame(setMainGrapf, columns=['timestemp', 'octets'])
+
+    dfMainGrapf['octets'] = dfMainGrapf['octets']
+    dfMainGrapf['octets'] = dfMainGrapf['octets'].apply(lambda x: round(x, 2))
+    print(dfMainGrapf)
+    dfMainGrapf = dfMainGrapf.reset_index().to_dict('records')
+    return dfMainGrapf
 
 origins = [
     "http://80.73.69.138:5432",
